241717cdcf0edae08712c0c99e8cccad01201254
Axel <daguerre.axel@gmail.com>
2026-05-14 23:32:05 +0200

    Ajout du tableau d'objet questions et nombre de questions selon la longueur du tableau

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,58 @@
 
 import { useState } from "react";
 
-//Fonction ajouter 1 point au score et afficher le score
+
 export default function Home() {
-  const [score, setScore] = useState(0);
+
+  const [playerAnswer, setPlayerAnswer] = useState("");
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  const questions = [
+    {
+      text: "Quelle est la capitale de la France ?",
+      answer: "Paris",
+      points: 1,
+      category: "Géographie"
+    },
+    {
+      text: "Quel est le résultat de 2 + 2 ?",
+      answer: "4",
+      points: 1,
+      category: "Mathématiques"
+    },
+    {
+      text: "Qui a écrit 'Les Misérables' ?",
+      answer: "Victor Hugo",
+      points: 1,
+      category: "Littérature"
+    },
+    {
+      text: "Quelle est la formule chimique de l'eau ?",
+      answer: "H2O",
+      points: 1,
+      category: "Sciences"
+    }
+  ];
+
+  function handleSubmit() {
+  console.log(playerAnswer);
+  setQuestionIndex(questionIndex + 1);
+}
 
   return (
     <main>
-      <h1>Score: {score}</h1>
-      <button onClick={() => setScore(score + 1)}>Ajouter 1 point</button>
+      <header>
+        <h1>ADA Quiz</h1>
+      </header>
+      
+      <h3>{questionIndex + 1}/{questions.length}</h3>
+
+      <h2>{questions[questionIndex].text}</h2>
+
+      <input type="text" value={playerAnswer} onChange={(event) => setPlayerAnswer(event.target.value)} />
+
+      <button onClick={handleSubmit}>Valider</button>
+
     </main>
   );
 }
